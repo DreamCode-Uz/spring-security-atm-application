890129c8787a19d98677ae2946c4278768f559d4
Dilshod Fayzullayev <fullstack.dev.uz@gmail.com>
2023-02-04 11:38:03 +0500

    :truck: renamed from ATMDTO.java

--- src/main/java/uz/pdp/springsecurityatm/payload/AtmDTO.java
+++ src/main/java/uz/pdp/springsecurityatm/payload/AtmDTO.java
@@ -11,7 +11,7 @@ import java.util.Set;
 import java.util.UUID;
 
 @Data
-public class ATMDTO {
+public class AtmDTO {
     @NotBlank
     private String street;
     @NotBlank
@@ -24,7 +24,9 @@ public class ATMDTO {
     @Min(0)
     private Double commission;
     @NotNull
-    private UUID userId;
+    private UUID userId; // Ushbu bankomatga javobgar shaxsning id raqami
+    private Double takeOut; // pul yechib olishning maximal miqdori kiritiladi
     private Set<Summa> sums;
     private Set<Dollar> dollars;
+    private Set<Integer> cardTypes; //  pul yechib olish uchun ruxsat etilgan karta tiplari kiritiladi
 }
